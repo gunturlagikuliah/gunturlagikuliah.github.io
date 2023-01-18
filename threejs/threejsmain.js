13372652bc830c022852fe904ad49225e8908484
gunturlagikuliah <117798039+gunturlagikuliah@users.noreply.github.com>
2023-01-18 12:09:40 +0700

    dah b8isa

--- threejs/threejsmain.js
+++ threejs/threejsmain.js
@@ -1,17 +1,23 @@
 import * as THREE from './three.module.js';
-console.log('THREE',THREE);
+//console.log('THREE',THREE);
 document.addEventListener("DOMContentLoaded", () => {
     const scene = new THREE.Scene();
 
     const geometry = new THREE.BoxGeometry(1 ,1 ,1);
-    const material = new THREE.MeshBasicMaterial({color: "#0000FF"});
+    const material = new THREE.MeshBasicMaterial({color: "#FFA07A"});
     const cube = new THREE.Mesh(geometry, material);
+   // const a = new THREE.Object3D;
+   // a.postion
 
-    console.log(cube.isObject3D);
+    //console.log(cube.isObject3D);
 
+    
+    //scene.children[0].position.set(0,0,-2);
+   // scene.children[0].rotation.set(0, Math.pi/4, 0);
+   
+    cube.position.set(0, 0, -2);
+    cube.rotation.set(0, Math.PI/4, 0);
     scene.add(cube);
-    cube.position.set(1,1,5);
-    cube.rotation.set(0, Math.pi/4, 0);
 
     const camera = new THREE.PerspectiveCamera();
     camera.position.set(1, 1, 5);
@@ -20,6 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
     renderer.setSize(500, 500);
     renderer.render(scene, camera);
     
+    document.body.appendChild(renderer.domElement);
     console.log("dom");
 }
 );
