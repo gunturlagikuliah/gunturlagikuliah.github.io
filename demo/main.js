8524f24fffe2b7539cb7e7ed2925e758d8da1674
gunturlagikuliah <117798039+gunturlagikuliah@users.noreply.github.com>
2023-03-16 12:02:39 +0700

    Update main.js
    Tambah 2 anchor buat scan 2 image tapi ini bergantian bukan bareng keluar objectnya

--- demo/main.js
+++ demo/main.js
@@ -13,8 +13,13 @@ document.addEventListener('DOMContentLoaded', () => {
         const geometry = new THREE.PlaneGeometry(1,1);
         const material = new THREE.MeshBasicMaterial({color: 0x0000FF, transparent: true, opacity:0.5});
         const plane = new THREE.Mesh(geometry, material);
+        const geometry2 = new THREE.PlaneGeometry(1,1);
+        const material2 = new THREE.MeshBasicMaterial({color: 0x00FF66, transparent: true, opacity:0.5});
+        const plane2 = new THREE.Mesh(geometry2, material2);
         const anchor = mindarThree.addAnchor(0);
+        const anchor2 = mindarThree.addAnchor(1);
         anchor.group.add(plane); // Ini hirarki grup 3js
+        anchor2.group.add(plane2);
         await mindarThree.start()
         renderer.setAnimationLoop(()=>{
             renderer.render(scene,camera);
