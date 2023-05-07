9c185031d5e6979ac4a116c5bef19d4d18dcf625
gunturlagikuliah <117798039+gunturlagikuliah@users.noreply.github.com>
2023-05-07 10:00:27 +0700

    Nambah komponen CSS

--- demo/maincoba.js
+++ demo/maincoba.js
@@ -20,48 +20,54 @@ document.addEventListener('DOMContentLoaded', () => {
         //Deklarasi renderer dan scene untuk mindar
         const { renderer, cssRenderer, scene, cssScene, camera } = mindarThree;
 
-        // NAVIGASI ATAS----
+        // LOKASI DAN NAVIGASI ATAS----
         const navigasiatas = new CSS3DObject(document.querySelector("#navigasiatas"));  //Membuat CCS 3D OBJECT dalam html dengan id
-        navigasiatas.scale.set(1,1,1);
-        //obj.position.set(0,-1500,0);
-        const navigasiAtasAnchor = mindarThree.addCSSAnchor(0);
-        const navigasiAtasAnchor2 = mindarThree.addCSSAnchor(1);
+        navigasiatas.scale.set(1, 1, 1);
+        navigasiatas.position.set(0, 450, 0);
+        const anchorMarker1 = mindarThree.addCSSAnchor(0);
+        const anchorMarker2 = mindarThree.addCSSAnchor(1);
 
+        //TENGAH----
 
+        const tengah = new CSS3DObject(document.querySelector("#tengah"));  //Membuat CCS 3D OBJECT dalam html dengan id
+        //tengah.scale.set(1.07,1.07,1.07);
+        tengah.scale.set(1.33, 1.33, 1.33);
+        tengah.position.set(0, 0, 0);
 
-
+        
         //NAVIGASI BAWAH----
 
-        //TENGAH
-
         //CALON KIRI----
+        const calonKiri = new CSS3DObject(document.querySelector("#calonkiri"));
 
         //CALON KANAN---
 
         //Debug target found
         //Anchor 1
-        navigasiAtasAnchor.onTargetFound = () => {
+        anchorMarker1.onTargetFound = () => {
             console.log("CSS TARGET FOUND");
-            document.querySelector('#isinavigasiatas').innerHTML="NARUTO";
-            navigasiAtasAnchor.group.add(navigasiatas);
-            
+            document.querySelector('#isinavigasiatas').innerHTML = "NARUTO";
+            anchorMarker1.group.add(navigasiatas);
+            anchorMarker1.group.add(tengah);
+            anchorMarker1.group.add(calonKiri);
+
         }
-        navigasiAtasAnchor.onTargetLost = () => {
+        anchorMarker1.onTargetLost = () => {
             console.log("CSS TARGET LOST");
         }
 
         //Anchor 2
-        navigasiAtasAnchor2.onTargetFound = () => {
+        anchorMarker2.onTargetFound = () => {
             console.log("CSS 2 TARGET FOUND");
-            document.querySelector('#isinavigasiatas').innerHTML="SASUKE";
-            navigasiAtasAnchor2.group.add(navigasiatas);
+            document.querySelector('#isinavigasiatas').innerHTML = "SASUKE";
+            anchorMarker2.group.add(navigasiatas);
 
         }
-        navigasiAtasAnchor2.onTargetLost = () => {
+        anchorMarker2.onTargetLost = () => {
             console.log("CSS 2 TARGET LOST");
         }
 
-       
+
 
 
         //Perintah render object mindAR dengan ketentuan renderer dan kamera mana yang digunakan
