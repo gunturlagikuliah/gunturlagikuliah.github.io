089635513ff21481f69c2e9a83fa9888b90a6495
gunturlagikuliah <117798039+gunturlagikuliah@users.noreply.github.com>
2023-06-23 21:59:45 +0700

    Test border

---
+++ semhas/test1.js
@@ -0,0 +1,12 @@
+import data from './data.json' assert {type:"json"};
+console.log(data);
+// document.querySelector('#container').innerHTML='\
+// sadasd <br> \
+// asdasd\
+// asdadsads <br>\
+// <button></button>\
+// ';
+document.querySelector('#container').innerHTML=`
+test<br>
+<img src='${data.calon[0].logokampanye}' alt='cambar'></img>
+`;
