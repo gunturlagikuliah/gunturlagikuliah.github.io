354988624bf7ff0c6bc35c857da719e9d1e6c251
gunturlagikuliah <117798039+gunturlagikuliah@users.noreply.github.com>
2023-07-17 18:56:30 +0700

    Update main.js
    Tambah Data sesuai sequence + test import three

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-import * as THREE from 'three';
+// import * as THREE from 'three';
 import { MindARThree } from 'mindar-image-three';
 import { mockWithVideo } from './container-mock.js';
 import { CSS3DObject } from 'three/addons/renderers/CSS3DRenderer.js';
@@ -29,55 +29,57 @@ document.addEventListener('DOMContentLoaded', () => {
             console.log(i);
             anchor[i].onTargetFound = () => {
                 console.log(`TARGET ${i} FOUND`);
+                //DATA
+                let dataCalon = data.calon[i];
 
                 //Button
                 document.getElementById('navigasi').style.display="block";
 
                 //LOGO
-                document.getElementById('logocalon').src=data.calon[i].logokampanye;
+                document.getElementById('logocalon').src=dataCalon.logokampanye;
 
                 //Shildshow
-                // document.getElementById('gambarshow1').src=data.calon[i].fotokampanye[0];
-                // document.getElementById('gambarshow2').src=data.calon[i].fotokampanye[1];
-                // document.getElementById('gambarshow3').src=data.calon[i].fotokampanye[2];
+                // document.getElementById('gambarshow1').src=dataCalon.fotokampanye[0];
+                // document.getElementById('gambarshow2').src=dataCalon.fotokampanye[1];
+                // document.getElementById('gambarshow3').src=dataCalon.fotokampanye[2];
 
-                // document.getElementById('gambarkecilshow1').src=data.calon[i].fotokampanye[0];
-                // document.getElementById('gambarkecilshow2').src=data.calon[i].fotokampanye[1];
-                // document.getElementById('gambarkecilshow3').src=data.calon[i].fotokampanye[2];
+                // document.getElementById('gambarkecilshow1').src=dataCalon.fotokampanye[0];
+                // document.getElementById('gambarkecilshow2').src=dataCalon.fotokampanye[1];
+                // document.getElementById('gambarkecilshow3').src=dataCalon.fotokampanye[2];
 
 
                 // LINK
-                document.getElementById('youtube').src=`https://www.youtube.com/embed/${data.calon[i].youtube.slice(32)}`;
-                // document.getElementById('videoprofil').href = data.calon[i].youtube;
-                document.getElementById('situsresmi').href = data.calon[i].web;
+                document.getElementById('youtube').src=`https://www.youtube.com/embed/${dataCalon.youtube.slice(32)}`;
+                // document.getElementById('videoprofil').href = dataCalon.youtube;
+                document.getElementById('situsresmi').href = dataCalon.web;
 
                 // VISI MISI
-                document.getElementById('visi').innerHTML = data.calon[i].visi;
+                document.getElementById('visi').innerHTML = dataCalon.visi;
                 document.getElementById('misi').innerHTML ='';
-                for (let j = 0; j < data.calon[i].misi.length; j++) {
-                    document.getElementById('misi').innerHTML += `<li>${data.calon[i].misi[j]}</li>`
+                for (let j = 0; j < dataCalon.misi.length; j++) {
+                    document.getElementById('misi').innerHTML += `<li>${dataCalon.misi[j]}</li>`
                 }
 
                 // CALON KEPALA
                 document.getElementById('nomorkepala').innerHTML = `<b>CALON PRESIDEN RI NO. 0${i + 1}</b>`;
-                document.getElementById('fotocalonkepala').src = data.calon[i].calonkepala.foto;
-                document.getElementById('namakepala').innerHTML = data.calon[i].calonkepala.nama;
-                document.getElementById('ttlkepala').innerHTML = `${data.calon[i].calonkepala.tempatlahir}, ${data.calon[i].calonkepala.tanggallahir}`;
-                document.getElementById('fotopartaikepala').src = data.calon[i].calonkepala.partaiasal.gambarpartai;
+                document.getElementById('fotocalonkepala').src = dataCalon.calonkepala.foto;
+                document.getElementById('namakepala').innerHTML = dataCalon.calonkepala.nama;
+                document.getElementById('ttlkepala').innerHTML = `${dataCalon.calonkepala.tempatlahir}, ${dataCalon.calonkepala.tanggallahir}`;
+                document.getElementById('fotopartaikepala').src = dataCalon.calonkepala.partaiasal.gambarpartai;
                 document.getElementById('sorotankepala').innerHTML='';
-                for (let j = 0; j < data.calon[i].calonkepala.sorotan.length; j++) {
-                    document.getElementById('sorotankepala').innerHTML += `<li>${data.calon[i].calonkepala.sorotan[j]}</li>`
+                for (let j = 0; j < dataCalon.calonkepala.sorotan.length; j++) {
+                    document.getElementById('sorotankepala').innerHTML += `<li>${dataCalon.calonkepala.sorotan[j]}</li>`
                 }
 
                 // CALON WAKIL
                 document.getElementById('nomorwakil').innerHTML = `<b>CALON WAKIL PRESIDEN RI NO. 0${i + 1}</b>`;
-                document.getElementById('fotocalonwakil').src = data.calon[i].calonwakil.foto;
-                document.getElementById('namawakil').innerHTML = data.calon[i].calonwakil.nama;
-                document.getElementById('ttlwakil').innerHTML = `${data.calon[i].calonwakil.tempatlahir}, ${data.calon[i].calonwakil.tanggallahir}`;
-                document.getElementById('fotopartaiwakil').src = data.calon[i].calonwakil.partaiasal.gambarpartai;
+                document.getElementById('fotocalonwakil').src = dataCalon.calonwakil.foto;
+                document.getElementById('namawakil').innerHTML = dataCalon.calonwakil.nama;
+                document.getElementById('ttlwakil').innerHTML = `${dataCalon.calonwakil.tempatlahir}, ${dataCalon.calonwakil.tanggallahir}`;
+                document.getElementById('fotopartaiwakil').src = dataCalon.calonwakil.partaiasal.gambarpartai;
                 document.getElementById('sorotanwakil').innerHTML='';
-                for (let j = 0; j < data.calon[i].calonwakil.sorotan.length; j++) {
-                    document.getElementById('sorotanwakil').innerHTML += `<li>${data.calon[i].calonwakil.sorotan[j]}</li>`
+                for (let j = 0; j < dataCalon.calonwakil.sorotan.length; j++) {
+                    document.getElementById('sorotanwakil').innerHTML += `<li>${dataCalon.calonwakil.sorotan[j]}</li>`
                 }
 
                 anchor[i].group.add(pageObject);
